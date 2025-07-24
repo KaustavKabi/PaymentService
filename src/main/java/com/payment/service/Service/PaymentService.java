0b5a4b84580ebba3427d314976b4c1bd423a4443
Thahaseem <90716673+Thahaseem@users.noreply.github.com>
2025-07-24 23:07:41 +0530

    Update PaymentService.java

--- src/main/java/com/payment/service/Service/PaymentService.java
+++ src/main/java/com/payment/service/Service/PaymentService.java
@@ -1,11 +1,14 @@
 package com.payment.service.Service;
 
 import com.payment.service.Entity.Payment;
+import com.payment.service.Enum.PaymentMethodEnum;
+import com.payment.service.Enum.PaymentStatusEnum;
 import com.payment.service.Repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,7 +25,22 @@ public class PaymentService {
     }
 
 
-    public Payment CreatePayment(Payment payment) {
+    public Payment initiatePayment(Payment paymentJson) {
+
+//        String OrderServiceUrl = "http://localhost:8081/api/orders/" + paymentJson.getOrderId();
+//        Payment payment1 = restTemplate.getForObject(OrderServiceUrl, Payment.class);
+
+//        if (payment1 == null) {
+//            throw new RuntimeException("order not found.");
+//        }
+
+        Payment payment = new Payment();
+        payment.setPaymentStatus(PaymentStatusEnum.INITIATED);
+        payment.setPaymentMethod(paymentJson.getPaymentMethod());
+        payment.setAmount(paymentJson.getAmount());
+        payment.setTimestamp(new Date());
+        payment.setOrderId(paymentJson.getOrderId());
+
         return paymentRepository.save(payment);
     }
 
@@ -36,6 +54,10 @@ public class PaymentService {
 
     public void cancelPayment(Long id) {
         paymentRepository.deleteById(id);
+    }
+
+    public Payment statusRetrieval(Payment payment) {
+        return paymentRepository.save(payment);
     }
 
 }
