d0180fb814605822e649f861b6dd1e5c9e27d344
Thahaseem <90716673+Thahaseem@users.noreply.github.com>
2025-07-31 20:55:26 +0530

    Update PaymentRepository.java

--- src/main/java/com/payment/service/Repository/PaymentRepository.java
+++ src/main/java/com/payment/service/Repository/PaymentRepository.java
@@ -3,6 +3,10 @@ package com.payment.service.Repository;
 import com.payment.service.Entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaymentRepository extends JpaRepository<Payment, Long>{
+        List<Payment> findByOrderId(Long orderId);
+
 }
 
